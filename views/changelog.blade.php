b6cab07875ffc98be755fbb7ab45a2c6ee43c8b9
Bernd Bestel <bernd@berrnd.de>
2019-09-22 13:17:13 +0200

    Added anchor links to changelog sections & small style changes for the changelog page

--- views/changelog.blade.php
+++ views/changelog.blade.php
@@ -22,14 +22,14 @@
 
 	@php $Parsedown = new Parsedown(); @endphp
 	@foreach($changelog['changelog_items'] as $changelogItem)
-	<section class="row align-items-center d-flex my-3">
-		<div class="col-xs-12 col-lg-8 offset-lg-2">
+	<section id="v{{ $changelogItem['version'] }}" class="row align-items-center d-flex my-3">
+		<div class="col">
 			<div class="card">
 				<div class="card-header">
 					<a class="discrete-link" data-toggle="collapse-next" href="#">Version <span class="font-weight-bold">{{ $changelogItem['version'] }}</span> released on <span class="font-weight-bold">{{ $changelogItem['release_date'] }}</span></a>
 				</div>
 				<div class="collapse @if($changelogItem['release_number'] >= $changelog['newest_release_number'] - 4) show @endif">
-					<div class="card-body text-left flowtext major-info pb-0 px-1">
+					<div class="card-body text-left major-info pb-0 px-3">
 						{!! $Parsedown->text($changelogItem['body']) !!}
 					</div>
 					<div class="card-body pt-0">
