894fe4be6c64c6d2312526299a4e741f88058a56
Bernd Bestel <bernd@berrnd.de>
2020-04-12 14:59:20 +0200

    Add new link (closes #13, closes grocy/grocy#715)

--- views/links.blade.php
+++ views/links.blade.php
@@ -25,6 +25,13 @@
 		<div class="col-xs-12 col-lg-10 offset-lg-1">
 
 			<p>
+				<h5>How to install grocy on FreeNas FreeBSD 11.3 Jail using nginx</h5>
+				<a class="small" href="https://github.com/grocy/grocy/issues/715#issue-597535439" target="_blank"><i class="fas fa-external-link-alt"></i> https://github.com/grocy/grocy/issues/715#issue-597535439</a>
+			</p>
+
+			<hr class="mt-0 pb-0">
+
+			<p>
 				<h5>grocy >= 2.6.1 subdirectory installation (Apache)</h5>
 				<a class="small" href="https://github.com/grocy/grocy/issues/694#issue-592850273" target="_blank"><i class="fas fa-external-link-alt"></i> https://github.com/grocy/grocy/issues/694#issue-592850273</a>
 			</p>
