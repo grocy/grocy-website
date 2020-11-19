0f9fd24e66a3d8b5efcc22b790926d5759b69e59
Bernd Bestel <bernd@berrnd.de>
2020-11-19 19:07:33 +0100

    Added new add-on link (closes #22)

--- views/addons.blade.php
+++ views/addons.blade.php
@@ -4,7 +4,8 @@
 @section('lang', 'en')
 
 @section('headerAdditional')
-<link rel="canonical" href="https://grocy.info/addons">
+<link rel="canonical"
+	href="https://grocy.info/addons">
 @stop
 
 @section('content')
@@ -27,7 +28,9 @@
 			<p>
 				<h5>grocy-docker</h5>
 				ERP beyond your fridge - now containerized<br>
-				<a class="btn btn-secondary btn-sm" href="https://github.com/grocy/grocy-docker" target="_blank"><i class="fab fa-github"></i> GitHub</a>
+				<a class="btn btn-secondary btn-sm"
+					href="https://github.com/grocy/grocy-docker"
+					target="_blank"><i class="fab fa-github"></i> GitHub</a>
 			</p>
 
 			<hr class="mt-0 pb-0">
@@ -35,7 +38,9 @@
 			<p>
 				<h5>linuxserver/docker-grocy (LinuxServer.io)</h5>
 				A container for grocy - the ERP application for your kitchen<br>
-				<a class="btn btn-secondary btn-sm" href="https://github.com/linuxserver/docker-grocy" target="_blank"><i class="fab fa-github"></i> GitHub</a>
+				<a class="btn btn-secondary btn-sm"
+					href="https://github.com/linuxserver/docker-grocy"
+					target="_blank"><i class="fab fa-github"></i> GitHub</a>
 			</p>
 
 			<hr class="mt-0 pb-0">
@@ -43,7 +48,9 @@
 			<p>
 				<h5>k8s-grocy</h5>
 				Run grocy on a Kubernetes Cluster<br>
-				<a class="btn btn-secondary btn-sm" href="https://git.sr.ht/~johnhamelink/k8s-grocy/" target="_blank"><i class="fas fa-external-link-alt"></i> Source</a>
+				<a class="btn btn-secondary btn-sm"
+					href="https://git.sr.ht/~johnhamelink/k8s-grocy/"
+					target="_blank"><i class="fas fa-external-link-alt"></i> Source</a>
 			</p>
 
 			<hr class="mt-0 pb-0">
@@ -51,7 +58,9 @@
 			<p>
 				<h5>grocy-android</h5>
 				grocy on your smartphone - with barcode scanning and batch processing<br>
-				<a class="btn btn-secondary btn-sm" href="https://github.com/patzly/grocy-android" target="_blank"><i class="fab fa-github"></i> GitHub</a>
+				<a class="btn btn-secondary btn-sm"
+					href="https://github.com/patzly/grocy-android"
+					target="_blank"><i class="fab fa-github"></i> GitHub</a>
 			</p>
 
 			<hr class="mt-0 pb-0">
@@ -59,7 +68,9 @@
 			<p>
 				<h5>Pantry Party</h5>
 				grocy on your smartphone - multiple barcode lookup sources, text-to-speech and more<br>
-				<a class="btn btn-secondary btn-sm" href="https://github.com/PantryParty/pantry_party" target="_blank"><i class="fab fa-github"></i> GitHub</a>
+				<a class="btn btn-secondary btn-sm"
+					href="https://github.com/PantryParty/pantry_party"
+					target="_blank"><i class="fab fa-github"></i> GitHub</a>
 			</p>
 
 			<hr class="mt-0 pb-0">
@@ -67,15 +78,29 @@
 			<p>
 				<h5>Barcode Buddy</h5>
 				A barcode scanning helper / lookup tool for grocy<br>
-				<a class="btn btn-secondary btn-sm" href="https://github.com/Forceu/barcodebuddy" target="_blank"><i class="fab fa-github"></i> GitHub</a>
+				<a class="btn btn-secondary btn-sm"
+					href="https://github.com/Forceu/barcodebuddy"
+					target="_blank"><i class="fab fa-github"></i> GitHub</a>
 			</p>
 
 			<hr class="mt-0 pb-0">
 
 			<p>
 				<h5>Home Assistant Add-On</h5>
-				 Home Assistant add-on for grocy<br>
-				<a class="btn btn-secondary btn-sm" href="https://github.com/hassio-addons/addon-grocy" target="_blank"><i class="fab fa-github"></i> GitHub</a>
+				Home Assistant add-on for grocy<br>
+				<a class="btn btn-secondary btn-sm"
+					href="https://github.com/hassio-addons/addon-grocy"
+					target="_blank"><i class="fab fa-github"></i> GitHub</a>
+			</p>
+
+			<hr class="mt-0 pb-0">
+
+			<p>
+				<h5>grocy iOS Shortcut (RoutineHub)</h5>
+				Complete tasks on the fly<br>
+				<a class="btn btn-secondary btn-sm"
+					href="https://routinehub.co/shortcut/7061/"
+					target="_blank"><i class="fas fa-external-link-alt"></i> RoutineHub</a>
 			</p>
 
 			<hr class="mt-0 pb-0">
@@ -83,7 +108,9 @@
 			<p>
 				<h5>pygrocy</h5>
 				A grocy Python API client<br>
-				<a class="btn btn-secondary btn-sm" href="https://github.com/SebRut/pygrocy" target="_blank"><i class="fab fa-github"></i> GitHub</a>
+				<a class="btn btn-secondary btn-sm"
+					href="https://github.com/SebRut/pygrocy"
+					target="_blank"><i class="fab fa-github"></i> GitHub</a>
 			</p>
 
 			<hr class="mt-0 pb-0">
@@ -91,7 +118,9 @@
 			<p>
 				<h5>pygrocydm</h5>
 				A python client for grocy (data management only) <br>
-				<a class="btn btn-secondary btn-sm" href="https://github.com/BlueBlueBlob/pygrocydm" target="_blank"><i class="fab fa-github"></i> GitHub</a>
+				<a class="btn btn-secondary btn-sm"
+					href="https://github.com/BlueBlueBlob/pygrocydm"
+					target="_blank"><i class="fab fa-github"></i> GitHub</a>
 			</p>
 
 			<hr class="mt-0 pb-0">
@@ -99,11 +128,13 @@
 			<p>
 				<h5>grocy-pyscanner</h5>
 				A python based barcode scanning helper<br>
-				<a class="btn btn-secondary btn-sm" href="https://github.com/grocy/grocy-pyscanner" target="_blank"><i class="fab fa-github"></i> GitHub</a>
+				<a class="btn btn-secondary btn-sm"
+					href="https://github.com/grocy/grocy-pyscanner"
+					target="_blank"><i class="fab fa-github"></i> GitHub</a>
 			</p>
 
 		</div>
 
-	</div>	
+	</div>
 </div>
 @stop
